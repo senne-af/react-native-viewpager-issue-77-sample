a225b82c81d775a3975d115e4695dc128aa5e697
Senne Van Cauwenberge <senne.vancauwenberge@appfoundry.be>
2019-12-11 11:43:29 +0100

    Added working workaround

--- App.js
+++ App.js
@@ -5,20 +5,28 @@ import ViewPager from '@react-native-community/viewpager'
 
 const App = () => {
 
-  const viewRef = useRef()
+  const viewRefOne = useRef()
+  const viewRefTwo = useRef()
 
   useEffect(() => {
-    console.log(viewRef)
-  }, [viewRef])
+    // will be undefined
+    console.log('one', viewRefOne)
+
+    //works
+    console.log('two', viewRefTwo)
+  }, [])
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <ViewPager style={{ flex: 1 }}>
-        <View ref={viewRef}>
+        <View ref={viewRefOne}>
           <Text>Screen 1</Text>
         </View>
-        <View><Text>Screen 2</Text></View>
-        <View><Text>Screen 3</Text></View>
+        <View>
+          <View ref={viewRefTwo}>
+            <Text>Screen 2</Text>
+          </View>
+        </View>
       </ViewPager>
     </SafeAreaView>
   )
